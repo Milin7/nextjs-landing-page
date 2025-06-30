dd0bd5d2eacc426ebed627ffb064aa5351596461
Isaac Martinez <isaac_martinez.g@hotmail.com>
2025-06-29 18:50:14 -0600

    feat: Implement sticky sideNav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,36 @@
-import { NavBarLinks } from "./NavBarLinks";
+import Link from "next/link";
+import { sections } from "../data/db";
+import HeadingTitle from "./HeadingTitle";
+import Typography from "./Typography";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   return (
-    <div>
-      <NavBarLinks />;
+    <div className="sticky top-0 h-screen">
+      <div className="flex justify-center flex-col items-center">
+        <HeadingTitle />
+        <nav>
+          <ul>
+            {sections.map((section) => (
+              <li key={section.id}>
+                <Link href={`#${section.title}`}>
+                  <Typography
+                    className={styles.container}
+                    variant="start"
+                    component="div"
+                    mt="8"
+                  >
+                    <div className="link-left-line border-t-1 border-landing-neon mt-4 w-20 mr-2"></div>
+                    <Typography component="h3" variant="heading4" color="neon">
+                      {section.title}
+                    </Typography>
+                  </Typography>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
